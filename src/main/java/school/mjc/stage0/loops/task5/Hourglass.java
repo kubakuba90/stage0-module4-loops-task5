ca6fbd7091571d9f865a15bba01d417e17c7a1e8
Jakub M <fotograflubin@gmail.com>
2022-07-06 12:21:53 +0200

    done

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,108 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+
+        if(height%2==0) {
+            hourGlassEven(height/2);
+        }
+        else {
+            hourGlassOdd((height/2)+1);
+        }
+
+
+    }
+
+    public static void hourGlassOdd(int nRows)
+    {
+        int i,j,k,l;
+
+        for(i=0;i<nRows;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=0;k<nRows-i;k++)
+            {
+                System.out.print("8");
+            }
+            for(l=0;l<nRows-i-1;l++)
+            {
+                System.out.print("8");
+            }
+            for(int x = nRows - i; x < nRows; x++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for(i=0;i<nRows-1;i++)
+        {
+            for(j=0;j<nRows-i-2;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=0;k<i+2;k++)
+            {
+                System.out.print("8");
+            }
+            for(l=0;l<i+1;l++)
+            {
+                System.out.print("8");
+            }
+            // problemowy fragment
+            for(j=0;j<nRows-i-2;j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void hourGlassEven(int nRows)
+    {
+        int i,j,k,l;
+
+        for(i=0;i<nRows;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=0;k<nRows-i;k++)
+            {
+                System.out.print("8");
+            }
+            for(l=0;l<nRows-i;l++)
+            {
+                System.out.print("8");
+            }
+            for(int x = nRows - i; x < nRows; x++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for(i=0;i<nRows;i++)
+        {
+            for(j=0;j<nRows-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=0;k<i+1;k++)
+            {
+                System.out.print("8");
+            }
+            for(l=0;l<i+1;l++)
+            {
+                System.out.print("8");
+            }
+            // problemowy fragment
+            for(j=0;j<nRows-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,36 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
 
+
+    public void printTriangle(int cathetusLength) {
+        int i, j;
+
+        // outer loop to handle number of rows
+        //  n in this case
+        for(i=0; i<cathetusLength; i++)
+        {
+
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+
+
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for(j=0; j<=i; j++)
+            {
+                // printing stars
+                System.out.print("8");
+            }
+            for(j=1*(cathetusLength-i); j-2 >= 0; j--)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
     }
+
 }
